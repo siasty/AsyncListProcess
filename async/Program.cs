308492b7f2efb6ddd3edb026e0a075ce4c7e2d3d
siasty <siasty3@gmail.com>
2018-11-28 15:38:27 +0100

    waitforexitasyc

--- async/Program.cs
+++ async/Program.cs
@@ -7,16 +7,15 @@ using System.Threading.Tasks;
 
 namespace async
 {
-    class Program
+    static class Program
     {
 
         static async Task Main(string[] args)
         {
-
             try
             {
-                await AccessTheProcesAsync();
-                Console.WriteLine("\r\nComplete.");
+               await AccessTheProcesAsync();
+               Console.WriteLine("\r\nComplete.");
             }
             catch (OperationCanceledException)
             {
@@ -32,54 +31,55 @@ namespace async
             Console.ReadKey();
         }
 
-        class MyProcess : Process
-        {
-            public void Stop(TaskCompletionSource<int> tcs,int lp)
-            {
-                Console.WriteLine("LP: {2}\t Total time:    {0} sec.\t\t Exit code:    {1}\r\n", this.TotalProcessorTime.TotalSeconds, this.ExitCode,lp);
-
-                tcs.SetResult(this.ExitCode);
-                this.Dispose();
-                this.Close();
-                OnExited();
-
-            }
-        }
         class CMD
         {
             public int lp { get; set; }
             public string cmd { get; set; }
             public string arg { get; set; }
         }
-        public static Task<int> RunProcessAsync(string fName, string arg = null, int lp = 1)
+        public static async Task<int> RunProcessAsync(string fName, string arg = null, int lp = 1)
         {
             var tcs = new TaskCompletionSource<int>();
-
-            var proc = new MyProcess();
             try
-            {    
-                    proc.StartInfo = new ProcessStartInfo
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
                     {
                         FileName = fName,
                         Arguments = arg,
-                        Verb = string.Format("Command_{0}",lp),
                         UseShellExecute = false,
                         RedirectStandardOutput = false,
                         CreateNoWindow = true
-                    };
-               
-                proc.Start();
-                proc.WaitForExit();
-                proc.Stop(tcs,lp);
+                    },
+                    EnableRaisingEvents = true
+                };
 
+                process.Exited += (sender, args) =>
+                {
+                    Console.WriteLine("LP: {2}\t Process Id: {3}\t Total time:    {0} sec.\t\t Exit code:    {1}\r\n", process.TotalProcessorTime.TotalSeconds, process.ExitCode, lp, process.Id);
+                    if (process.ExitCode != 0)
+                    {
+                        tcs.SetException(new InvalidOperationException("The process did not exit correctly."));
+                        process.Dispose();
+                    }
+                    else
+                    {
+                        tcs.SetResult(process.ExitCode);
+                        process.Dispose();
+                    }
+                };
+
+                process.Start();
+                await process.WaitForExitAsync();
             }
             catch (Exception ex)
             {
                 tcs.SetException(ex);
-                Console.WriteLine("LP: {0} \t Error: {1}\r\n", lp, ex.Message);
             }
 
-            return tcs.Task;
+            return await tcs.Task;
+
         }
         private static List<CMD> CommandList()
         {
@@ -87,33 +87,59 @@ namespace async
             {
                new CMD{ lp = 1 ,cmd = "cmd", arg  = "/C dir"},
                new CMD{ lp = 2 ,cmd = "cmd", arg  = "/C dir"},
-               new CMD{ lp = 3 ,cmd = "cmds", arg  = "/C dir"},
+               new CMD{ lp = 3 ,cmd = "cmd", arg  = "/C timeout 10"},
                new CMD{ lp = 4 ,cmd = "cmd", arg  = "/C dir"},
                new CMD{ lp = 5 ,cmd = "cmd", arg  = "/C dir"},
             };
             return cmd;
         }
-        static async Task AccessTheProcesAsync()
+        private static async Task AccessTheProcesAsync()
         {
  
             List<CMD> CmdlList = CommandList();
 
             IEnumerable<Task<int>> downloadTasksQuery =
-                from url in CmdlList select RunProcessAsync(url.cmd, url.arg, url.lp);
+                from url in CmdlList select RunProcessAsync(url.cmd, url.arg, url.lp) ;
 
+            
             List<Task<int>> downloadTasks = downloadTasksQuery.ToList();
 
             while (downloadTasks.Count > 0)
             {
-                // Identify the first task that completes.
                 Task<int> firstFinishedTask = await Task.WhenAny(downloadTasks);
-
-                // ***Remove the selected task from the list so that you don't
                 downloadTasks.Remove(firstFinishedTask);
-
-                // Await the completed task.
                 await firstFinishedTask;
             }
         }
+        public static async Task WaitForExitAsync(this Process process, CancellationToken cancellationToken = default)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            void Process_Exited(object sender, EventArgs e)
+            {
+                tcs.TrySetResult(true);
+            }
+
+            process.EnableRaisingEvents = true;
+            process.Exited += Process_Exited;
+
+            try
+            {
+                if (process.HasExited)
+                {
+                    return;
+                }
+
+                using (cancellationToken.Register(() => tcs.TrySetCanceled()))
+                {
+                    await tcs.Task;
+                }
+            }
+            finally
+            {
+                process.Exited -= Process_Exited;
+            }
+        }
+
     }
 }
